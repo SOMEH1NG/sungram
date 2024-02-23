6ec30edae6e569b2a567803148f7a546217a965f
SOMEH1NG <158502800+SOMEH1NG@users.noreply.github.com>
2024-02-23 23:55:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name='sungram',
-    version='0.1',
-    packages=find_packages(),
+    name="sungram",
+    version="0.1",
+    description="A lightweight and asynchronous Python library for building Telegram bots.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Kunal Gaikwad",
+    author_email="kunalgaikwad9322@gmail.com",
+    packages=["sungram"],
     install_requires=[
-        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
     ],
 )
